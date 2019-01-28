2f1d9d81187f27b9640d3c1150af1876843b7528
YASIR FARHAN <38399184+yasirfarhan17@users.noreply.github.com>
2019-01-28 12:04:51 +0530

    Add files via upload

---
+++ Array_sort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Array_sort
+{
+public static void main(String args[])
+{
+String arr[]={"yasir","Abhishek","farhan"};
+for(int i=0;i<arr.length;i++)
+{
+if(arr[i].equals("yasir"))
+{
+System.out.print("**** ");
+}
+else if(arr[i].equals("farhan"))
+{
+System.out.print("####");
+}}
+}
+}
+

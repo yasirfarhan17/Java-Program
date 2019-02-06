551dad3bf3cda4756bcca09f37c74c0085163558
YASIR FARHAN <38399184+yasirfarhan17@users.noreply.github.com>
2019-02-07 01:55:01 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class swap{
+public static void main(String args[]){
+int a;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a value");
+a=sc.nextInt();
+if(a%2==0)
+System.out.println(a+" is an even number");
+else
+System.out.println(a+" is an odd number");
+}
+}
